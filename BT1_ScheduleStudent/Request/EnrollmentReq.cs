96a42088489b88cc9e4aef0991142269c5872c25
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2025-01-12 18:44:54 +0700

    Add API Enrollments
    GetAll_GetByID_Create

--- BT1_ScheduleStudent/Request/EnrollmentReq.cs
+++ BT1_ScheduleStudent/Request/EnrollmentReq.cs
@@ -1,9 +1,13 @@
-﻿namespace BT1_ScheduleStudent.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BT1_ScheduleStudent.Request
 {
     public class EnrollmentReq
     {
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Grade not null")]
         public string Grade { get; set; }
     }
 }
--- BT1_ScheduleStudent/Services/EnrollmentService.cs
+++ BT1_ScheduleStudent/Services/EnrollmentService.cs
@@ -1,4 +1,11 @@
-﻿namespace BT1_ScheduleStudent.Services
+﻿using System.Numerics;
+using BT1_ScheduleStudent.Model;
+using BT1_ScheduleStudent.Request;
+using BT1_ScheduleStudent.Response;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BT1_ScheduleStudent.Services
 {
     public class EnrollmentService
     {
@@ -8,5 +15,75 @@
         {
             _context = context;
         }
+
+        public async Task<IEnumerable<EnrollmentRes>> GetAllEnrollmentsAsync()
+        {
+            var enrollments = await _context.Enrollment.ToListAsync();
+
+            var enrollmentResponses = enrollments.Select(results => new EnrollmentRes
+            {
+                CourseID = results.CourseID,
+                EnrollmentID = results.EnrollmentID,
+                Grade = results.Grade,
+                StudentID = results.StudentID,
+            }).ToList();
+
+            return enrollmentResponses;
+        }
+
+        public async Task<EnrollmentRes> GetEnrollmentByIdAsync(int id)
+        {
+            var results = await _context.Enrollment
+                .Where(e => e.EnrollmentID == id)
+                .FirstOrDefaultAsync();
+            if(results == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new EnrollmentRes
+                {
+                    CourseID = results.CourseID,
+                    EnrollmentID = results.EnrollmentID,
+                    Grade = results.Grade,
+                    StudentID = results.StudentID,
+                };
+            }  
+        }
+
+        public async Task<EnrollmentRes> CreateEnrollmentAsync(EnrollmentReq req)
+        {
+            var courseExist = await _context.Course
+                              .AnyAsync(course => course.CourseID == req.CourseID);
+            if (!courseExist)
+            {
+                throw new ArgumentException($"Course with Id {req.CourseID} does not exist");
+            }
+            var studentExist = await _context.Student
+                                .AnyAsync(student => student.StudentID == req.StudentID);
+            if (!studentExist)
+            {
+                throw new ArgumentException($"Student with Id {req.StudentID} does not exist");
+            }
+            var enrollment = new Enrollment
+            {
+                CourseID = req.CourseID,
+                StudentID = req.StudentID,
+                Grade = req.Grade,
+            };
+
+            _context.Enrollment.Add(enrollment);
+            await _context.SaveChangesAsync();
+
+            return new EnrollmentRes
+            {
+                EnrollmentID = enrollment.EnrollmentID,
+                CourseID = enrollment.CourseID,
+                StudentID = enrollment.StudentID,
+                Grade = enrollment.Grade,
+            };
+        }
+
     }
 }
