d4c2bf2a8f319f048c830fa76780c954827c9f62
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2025-01-12 18:44:54 +0700

    Add API Enrollments
    GetAll_GetByID_Create

--- BT1_ScheduleStudent/Controllers/EnrollmentsController.cs
+++ BT1_ScheduleStudent/Controllers/EnrollmentsController.cs
@@ -1,4 +1,6 @@
-﻿using BT1_ScheduleStudent.Services;
+﻿using BT1_ScheduleStudent.Request;
+using BT1_ScheduleStudent.Response;
+using BT1_ScheduleStudent.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,29 @@ namespace BT1_ScheduleStudent.Controllers
         public EnrollmentsController(EnrollmentService EnrollmentService)
         {
             _EnrollmentService = EnrollmentService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EnrollmentRes>>> GetAllEnrollments() 
+        {
+            var response = await _EnrollmentService.GetAllEnrollmentsAsync();
+            return Ok(new { status = 1, decription = response });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EnrollmentRes>> GetEnrollment(int id)
+        {
+            var response = await _EnrollmentService.GetEnrollmentByIdAsync(id);
+            if(response == null)
+            {
+                return NotFound();
+            }
+            return Ok(new { status = 1, decription = response });
+        }
+        [HttpPost]
+        public async Task<ActionResult<EnrollmentRes>> PostEnrollment(EnrollmentReq req)
+        {
+            var response = await _EnrollmentService.CreateEnrollmentAsync(req);
+         return Ok(new { status = 1, decription = response });
         }
     }
 }
