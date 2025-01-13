ba5c96a1f45eb90ccd2e70c2d71e7511323ab745
letanbao120924 <letanbao3@gmail.com>
2025-01-13 15:07:03 +0700

    GET ALL, GET ID, CREATE

--- BT1_ScheduleStudent/Controllers/StudentsController.cs
+++ BT1_ScheduleStudent/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
-﻿using BT1_ScheduleStudent.Services;
+﻿using BT1_ScheduleStudent.Request;
+using BT1_ScheduleStudent.Response;
+using BT1_ScheduleStudent.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,5 +16,37 @@ namespace BT1_ScheduleStudent.Controllers
         {
             _StudentService = StudentService;
         }
+
+        // GET: api/Students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentRes>>> GetAllStudents()
+        {
+            var students = await _StudentService.GetAllStudentsAsync();
+            return Ok(students);
+        }
+
+        // GET: api/Student/4
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentRes>> GetStudent(int id)
+        {
+            var response = await _StudentService.GetStudentByIdAsync(id);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            return response;
+        }
+
+        // POST: api/Student
+        [HttpPost]
+        public async Task<ActionResult<StudentRes>> PostStudent(StudentReq StudentReq)
+        {
+            var response = await _StudentService.CreateStudentAsync(StudentReq);
+            return Ok();
+        }
+
+        
     }
 }
