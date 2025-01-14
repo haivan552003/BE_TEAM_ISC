7ffffccb8c62aa327dab3ce2fdd1bbb980b79f6f
VanLTH <haivan5.5.2003@gmail.com>
2025-01-14 16:54:48 +0700

    update use swager

--- BT1_ScheduleStudent/Program.cs
+++ BT1_ScheduleStudent/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddScoped<EnrollmentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
